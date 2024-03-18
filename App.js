c6ec38ccba101755a01ca6397527ac1dfb9b5987
MecHR17 <asrin17cankaya@hotmail.com>
2024-03-18 13:52:23 +0300

    Added puzzle checker

--- App.js
+++ App.js
@@ -22,6 +22,19 @@ const styles = StyleSheet.create({
   selectedButton: {
     backgroundColor: 'blue', // Change the color when button is selected
   },
+  checkButton: {
+    backgroundColor: 'blue',
+    padding:15,
+    borderRadius:5,
+    marginHorizontal:100,
+    marginTop:10,
+  },
+  buttonText: {
+    color:'white',
+    fontWeight:'bold',
+    textAlign:'center',
+    fontSize:20
+  },
   sideText: {
     fontSize:15,
     fontWeight:'bold',
@@ -88,8 +101,70 @@ const SideNum = ({data}) => {
   />
 }
 
+const checkPuzzle= (board,topNums,sideNums,cols) => {
+  //RowCheck
+  for(var i = 0; i < cols; i++){
+    var cur = 0;
+    var tmp = [];
+    for(var k = i*cols; k < i*cols+cols; k++){
+      if(board[k]){
+        cur++;
+      }
+      else{
+        if(cur != 0){
+          tmp.push(cur);
+          cur = 0;
+        }
+      }
+    }
+    if(cur != 0){
+      tmp.push(cur);
+    }
+    if(tmp.toString() !== sideNums[i].toString()){
+      return false;
+    }
+  }
+  //ColCheck
+  for(var i=0; i<cols; i++){
+    var cur = 0;
+    var tmp2 = [];
+    for(var k = i; k<=cols*cols-(cols-i);k+=cols){
+      if(board[k]){
+        cur++;
+      }
+      else{
+        if(cur != 0){
+          tmp2.push(cur);
+          cur = 0;
+        }
+      }
+    }
+    if(cur != 0){
+      tmp2.push(cur);
+    }
+
+    if(tmp2.toString() !== topNums[i].toString()){
+      return false;
+    }
+  }
+
+  //If passed both checks
+  return true;
+}
+
+const handleCheck = (board,topData,sideData,colCount,setBtnText) => {
+  const res = checkPuzzle(board,topData,sideData,colCount);
+  if(res){
+    setBtnText("Solved!");
+  }
+  else{
+    setBtnText("Check");
+  }
+}
+
 export default function App() {
   const colCount = 5;
+  const [btnText,setBtnText] = useState("Check");
   const [selected, setSelected] = useState(null);
   const [board, setBoard] = useState(Array.from(
     {length: colCount*colCount},
@@ -101,18 +176,22 @@ export default function App() {
       (_,index) => {return index==item? !board[index]: board[index];}
     ));
   };
-
+  
   const data = Array.from({ length: colCount*colCount }, (_, index) => index); // Example data from 1 to 12
-  const sideData = [[1,1,1],[1,1,1],[1,3],[5],[2,1]];
-  const topData = [[1,1,1],[1,1,1],[1,3],[5],[2,1]];
+  const sideData = [[1,2],[1,3],[3],[5],[2,1]];
+  const topData = [[2,2],[3],[3],[5],[2,1]];
   return (
-    <View style={{flexDirection:'column', justifyContent:'flex-start'}}>
+    <View style={{flexDirection:'column', justifyContent:'flex-start',marginTop:30}}>
       <TopNum data={topData} colNum={colCount} />
       <View style={{flexDirection:'row', justifyContent:'flex-start'}}>
         <SideNum data={sideData}/>
         <GridButtons data={data} onPress={handleButtonPress}
         selected={selected} colCount={colCount} blacks={board}/>
       </View>
+      <TouchableOpacity style={styles.checkButton}
+      onPress={() => handleCheck(board,topData,sideData,colCount,setBtnText)}>
+        <Text style={styles.buttonText}>{btnText}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
